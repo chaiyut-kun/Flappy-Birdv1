77e9813037ce5863b46f5486d17b3ba240e76319
chaiyut-kun <chwork28@hotmail.com>
2024-06-29 23:52:50 +0700

    First commit

---
+++ Assets/movePipe_script.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePipe_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float bird_speed = 10;
+    public float deadzone = -45;
+    public Score_UI score_UI;
+    void Start()
+    {
+        score_UI = GameObject.FindGameObjectWithTag("logic").GetComponent<Score_UI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3.left * bird_speed) * Time.deltaTime;
+
+        if (transform.position.x < deadzone){
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
+        increaseSpeed();
+    }
+
+    void increaseSpeed(){
+        if (score_UI.getScore() % 2 == 0 && score_UI.getScore() != 0){
+            bird_speed+=0.001f;
+            Debug.Log(bird_speed    .ToString());
+        }   
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+" #Flappy Bird version1" 
